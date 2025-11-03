3046934ee2f87ec659621de38debd0fe76e7c253
yunjinjeong679 <yunjinjeong679@gmail.com>
2025-11-03 11:06:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.title ('나의 첫 웹 서비스 만들기!')
 st.write ('안녕하세요, 만나서 반갑습니다.')
 name=st.text_input('이름을 입력해주세요!')
-if st.butten( '인삿말 생성'):
+if st.button( '인삿말 생성'):
   st.write(name+'님 반가워용>_<')
-  st.ballons()
+  st.balloons()
